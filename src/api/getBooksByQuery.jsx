fe5398ac9c48724052e1e04225e19511b7838260
teo-crth <teoconrath@gmail.com>
2024-12-16 17:02:15 +0100

    api files

---
+++ src/api/getBooksByQuery.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+// Base URL de l'API Google Books
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+
+// Fonction pour rechercher des livres
+export const fetchBooks = async (query, maxResults = 50, startIndex = 0) => {
+  try {
+    // Requête avec axios
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: query, // Terme de recherche
+        maxResults,
+        startIndex,
+        printType: 'books',
+        langRestrict: 'fr' // Type de résultat à afficher
+      },
+    });
+
+    // Filtrer les résultats pour exclure les ebooks
+    const physicalBooks = (response.data.items || []).filter(
+        (book) => book.saleInfo.isEbook === false // Vérifie si le livre n'est pas un ebook
+    );
+  
+    return physicalBooks;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des livres:', error);
+    throw error; // Rejetez l'erreur pour la gestion
+  }
+};
---
+++ src/api/getOneBook.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// Base URL de l'API Google Books
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+
+// Fonction pour rechercher des livres
+export const fetchBooks = async (bookId) => {
+  try {
+    // Requête avec axios
+    const response = await axios.get(BASE_URL/{bookId});
+
+    return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération du livre:', error);
+    throw error; // Rejetez l'erreur pour la gestion
+  }
+};
