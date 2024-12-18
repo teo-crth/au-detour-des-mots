a095aac9a07f31324dd4f836bee8b4c25822a3b9
AlexNesvit <alex.nesvit2000@gmail.com>
2024-12-18 11:36:25 +0100

    BookPage.css

--- src/pages/book/BookPage.jsx
+++ src/pages/book/BookPage.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+//import { allBooks } from '../../utils/allBooksArray';
+import './BookPage.css'; 
 
 const BookPage = () => {
-  const { id } = useParams(); // id URL
+  const { id } = useParams();
+  const book = allBooks.find((b) => b.id === parseInt(id));
+  const [likes, setLikes] = useState(0);
+
+  const placeholderImage = '/images/placeholder.jpg';
+
+  if (!book) {
+    return <h1>Livre non trouvé</h1>;
+  }
 
   return (
-    <div>
-      <h1>Page detaille du livre</h1>
-      <p>Livre ID: {id}</p>
-      {/* Les donnes de la book */}
+    <div className="book-page">
+      <h1>{book.title}</h1>
+      <img src={book.image || placeholderImage} alt={book.title} />
+      <p>
+        <strong>Author:</strong> {book.author}
+      </p>
+      <p>
+        <strong>Pages:</strong> {book.pages}
+      </p>
+      <p>
+        <strong>Description:</strong> {book.description}
+      </p>
+      <div>
+        <button className="like-button" onClick={() => setLikes(likes + 1)}>
+          👍 Like {likes}
+        </button>
+      </div>
     </div>
   );
 };
