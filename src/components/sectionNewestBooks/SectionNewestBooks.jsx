ff0717cad519ff9565feb4b2ff4c7124cc7fb033
XPM56780 <xp.marechal@free.fr>
2024-12-19 17:01:35 +0100

    2th-MAJ

---
+++ src/components/sectionNewestBooks/SectionNewestBooks.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import allBooks from '../../utils/allBooksArray/allBooksArray';
+import './sectionNewestBooks.css';
+import BookCard from '../ui/bookCard/BookCard';
+
+const sectionNewestBooks = () => {
+    console.log('allBooks', allBooks);
+
+    // Filtrer les livres qui ont une date de publication
+    const filteredBooks = allBooks.filter((book) => {
+        return book.volumeInfo.publishedDate && book.volumeInfo.publishedDate[0]; // On s'assure que la date de publication existe et n'est pas vide
+    });
+
+    // Trier les livres par date de publication (les plus récentes en premier)
+    const sortedBooks = filteredBooks.sort((a, b) => {
+        const dateA = new Date(a.volumeInfo.publishedDate);
+        const dateB = new Date(b.volumeInfo.publishedDate);
+        return dateB - dateA; // Descendant
+    });
+
+
+    // Conserver les 10 premières entrées
+    const newestBooks = sortedBooks.slice(0, 10);
+
+    return (
+        <section>
+            <h2>les 10 dernières publications</h2>
+            <BookCard array={newestBooks} />
+
+        </section>
+    )
+
+
+}
+
+
+export default sectionNewestBooks;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Home.css';
 import SectionCategories from '../../components/sectionBookCategories/SectionCategories';
+import NewestBooks from '../../components/sectionNewestBooks/SectionNewestBooks';
 
 const Home = () => {
-    return (
-      <>
-        <h1>Au détour des mots</h1>
-        < SectionCategories />
-      </>
-    );
+  return (
+    <>
+      <h1>Au détour des mots</h1>
+      < NewestBooks />
+      < SectionCategories />
+    </>
+  );
 };
 
 export default Home;
