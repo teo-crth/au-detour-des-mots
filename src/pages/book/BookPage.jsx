f5024655b778a613f4b3fa0e3ea54d3050c2efe5
teo-crth <teoconrath@gmail.com>
2024-12-31 14:35:14 +0100

    fix if description of one book does not exist

--- src/pages/book/BookPage.jsx
+++ src/pages/book/BookPage.jsx
@@ -33,13 +33,13 @@ const BookPage = () => {
                     <div>
                         <Button text={isInArray(book) ? 'Ajouté !' : "Ajouter à ma liste"} onClick={isInArray(book) ? null : () => handleLike(book)} className={isInArray(book) ? "book-card-button-already-liked" : "book-card-button"} />
                     </div>
-                {isInArray(book) ?
-                    <span className='book-card__heart_icon' onClick={() => handleHeartClick(book)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                        </svg>
-                    </span>
-                    : null}
+                    {isInArray(book) ?
+                        <span className='book-card__heart_icon' onClick={() => handleHeartClick(book)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                            </svg>
+                        </span>
+                        : null}
                 </div>
                 <div className="container-book-info">
                     <h1>{book?.volumeInfo?.title}</h1>
@@ -53,8 +53,10 @@ const BookPage = () => {
                     <p className='paragraph-info-book'>{book?.volumeInfo?.pageCount} pages</p>
                     <p className='paragraph-info-book'>{book?.volumeInfo?.publishedDate}</p>
                 </div>
-                <h2>Résumé :</h2>
-                <p className='resume'>{book?.volumeInfo?.description}</p>
+                <div className="container-resume">
+                    <h2>Résumé :</h2>
+                    <p className='resume'>{book?.volumeInfo?.description || "Aucun résumé pour ce livre."}</p>
+                </div>
             </div>
         </div>
     );
