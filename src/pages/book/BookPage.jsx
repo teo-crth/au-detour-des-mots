9b6a88e554369d4bf2be2b4333f66e2cbfd33d60
teo-crth <teoconrath@gmail.com>
2024-12-31 14:30:31 +0100

    css icon heart

--- src/pages/book/BookPage.jsx
+++ src/pages/book/BookPage.jsx
@@ -28,18 +28,18 @@ const BookPage = () => {
     return (
         <div className="book-page">
             <div className="container-the-book">
-                    {isInArray(book) ?
-                        <span className='book-card__heart' onClick={() => handleHeartClick(book)}>
-                            <svg xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                            </svg>
-                        </span>
-                        : null}
                 <div className="container-img-bookPage">
                     <img className='book-img' src={book?.volumeInfo?.imageLinks?.thumbnail || placeholderImage} alt={`Image du livre ${book?.volumeInfo?.title}`} />
                     <div>
                         <Button text={isInArray(book) ? 'Ajouté !' : "Ajouter à ma liste"} onClick={isInArray(book) ? null : () => handleLike(book)} className={isInArray(book) ? "book-card-button-already-liked" : "book-card-button"} />
                     </div>
+                {isInArray(book) ?
+                    <span className='book-card__heart_icon' onClick={() => handleHeartClick(book)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                        </svg>
+                    </span>
+                    : null}
                 </div>
                 <div className="container-book-info">
                     <h1>{book?.volumeInfo?.title}</h1>
