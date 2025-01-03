70859d9018dee84089bd73d07f1e3d3dbf734f13
teo-crth <teoconrath@gmail.com>
2025-01-03 22:40:45 +0100

    path logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
 
     return (
         <header>
-            <NavLink to="/au-detour-des-mots" className={({ isActive }) => isActive ? 'active' : ''}><img src="./au-detour-des-mots/public/assets/images/logo-table.webp" alt="Logo table avec des livres" /></NavLink>
+            <NavLink to="/au-detour-des-mots" className={({ isActive }) => isActive ? 'active' : ''}><img src="/au-detour-des-mots/public/assets/images/logo-table.webp" alt="Logo table avec des livres" /></NavLink>
                 <nav className="nav-desktop">
                     <NavLink to="/au-detour-des-mots" className={({ isActive }) => isActive ? 'active' : ''}>Accueil</NavLink>
                     <NavLink to="/au-detour-des-mots/mes-livres" className={({ isActive }) => isActive ? 'active' : ''}>Mes livres</NavLink>
