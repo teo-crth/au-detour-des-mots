530ab6da4c0b8d4753a21b7d3a9842ab2263e6bc
Paul-LOMBARD <102720356+Paul-LOMBARD@users.noreply.github.com>
2025-01-07 11:55:52 +0100

    add random suggestion

--- src/components/sectionSuggestionBook/SectionSuggestionBook.jsx
+++ src/components/sectionSuggestionBook/SectionSuggestionBook.jsx
@@ -6,8 +6,12 @@ import './sectionSuggestionBook.css'
 const sectionSuggestionBook = ({ book }) => {
     const catBook = book.volumeInfo?.categories[0];
     const sameBook = allBooksArray.filter((books) => books.volumeInfo.categories == catBook).filter((books) => books.id != book.id);
-    const sameBookLimited = sameBook.slice(0, 3);
-    const randomIndex = Math.floor((Math.random() * sameBook.length) + 1)
+    /*const sameBookLimited = sameBook.slice(0, 3);*/
+
+    const randomIndex1 = Math.floor(Math.random() * sameBook.length) + 1;
+    const randomIndex2 = Math.floor(Math.random() * sameBook.length) + 1;
+    const randomIndex3 = Math.floor(Math.random() * sameBook.length) + 1;
+    const sameBookLimited = [sameBook[randomIndex1], sameBook[randomIndex2], sameBook[randomIndex3]];
     console.log(sameBookLimited);
 
     return (
