263b5ae45b9a885e1f1501d9e31a902abb257a24
teo-crth <teoconrath@gmail.com>
2025-01-08 10:59:16 +0100

    fix: darkmode mobile

--- src/components/ui/burgerMenu/BurgerMenu.jsx
+++ src/components/ui/burgerMenu/BurgerMenu.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import './BurgerMenu.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { AppContext } from '../../../context/context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const BurgerMenu = () => {
-    const { menuOpen, setMenuOpen } = useContext(AppContext);
+    const { menuOpen, setMenuOpen, isDarkMode, toggleTheme } = useContext(AppContext);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
@@ -24,7 +25,8 @@ const BurgerMenu = () => {
             <nav className='nav-mobile'>
                 <NavLink to="/au-detour-des-mots" className={({ isActive }) => isActive ? 'active' : ''} onClick={handleLink}>Accueil</NavLink>
                 <NavLink to="/au-detour-des-mots/mes-livres" className={({ isActive }) => isActive ? 'active' : ''} onClick={handleLink}>Mes livres</NavLink>
-                <NavLink to="/au-detour-des-mots//recherche" className={({ isActive }) => isActive ? 'active' : ''} onClick={handleLink}>Recherche</NavLink>
+                <NavLink to="/au-detour-des-mots/recherche" className={({ isActive }) => isActive ? 'active' : ''} onClick={handleLink}>Recherche</NavLink>
+                <FontAwesomeIcon className="dark-mode-icon" icon={faMoon} onClick={toggleTheme} />
             </nav>
         </div>
     );
