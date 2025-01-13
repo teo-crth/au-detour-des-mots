0fa0f7dc985abdaaddab1851ebcadb390f817bdb
teo-crth <teoconrath@gmail.com>
2025-01-13 09:03:40 +0100

    css dark-mode bookPage

--- src/pages/404/NoFoundPage.tsx
+++ src/pages/404/NoFoundPage.tsx
@@ -13,7 +13,7 @@ const NoFoundPage = () => {
     }
     return (
         <>
-            <div className='container'>
+            <div className='container-404'>
                 <h1>Page introuvable</h1>
                 <iframe src="https://giphy.com/embed/jU9OCvBiO1besabUKU" width="480" height="269" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
                 <h2>Vous êtes perdu ? Félicitation vous êtes un aventurier !</h2>
